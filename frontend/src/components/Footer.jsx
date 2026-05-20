46c17097b6e601d6a531831bb859b34615e7d007
Joris Barbin <barbin.joris@hotmail.fr>
2026-05-20 14:10:49 +0200

    Corrections header footer layout and index styles added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,8 @@
 function Footer() {
   return (
     <footer>
-        <img src="public/logo.png" alt="Logo Kasa" />
-      <p>&copy; 2023 Kasa. Tous droits réservés.</p>
-      <p>Contactez-nous : contact@kasa.fr</p>
-      <p>suivez-nous sur les réseaux sociaux</p>
-      <p>Facebook X Instagram</p>
+        <img src="/logo.png" alt="Logo Kasa" />
+      <p>&copy; 2020 Kasa. All rights reserved</p>
     </footer>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <header>
-        <img src="public/logo.png" alt="Logo Kasa" />
+        <img src="/logo.png" alt="Logo Kasa" />
         <h1>Kasa</h1>
         <nav>
           <Link to="/">Accueil</Link>
           <Link to="/about">À propos</Link>
-          <Link to="/housing">Logements</Link>
+
         </nav>
     </header>
   )
