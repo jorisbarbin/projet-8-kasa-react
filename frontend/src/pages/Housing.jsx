4ca08871f08924f3afe09a750d7c908a75951e7b
Joris Barbin <barbin.joris@hotmail.fr>
2026-05-27 16:54:08 +0200

    Handle property details and invalid IDs

--- frontend/src/pages/Housing.jsx
+++ frontend/src/pages/Housing.jsx
@@ -1,5 +1,38 @@
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Error from './Error'
+
 function Housing() {
-  return <h1>Page logement</h1>
+  const { id } = useParams()
+  const [property, setProperty] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/properties")
+      .then((response) => response.json())
+      .then((data) => {
+        const selectedProperty = data.find((property) => property.id === id)
+        setProperty(selectedProperty)
+        setIsLoading(false)
+      })
+  }, [id])
+
+  if (isLoading) {
+    return <p>Chargement en cours...</p>
+  }
+
+  if (!property) {
+    return <Error />
+  }
+
+  return (
+    <section className="housing">
+      <img src={property.cover} alt={property.title} />
+      <h1>{property.title}</h1>
+      <p>{property.location}</p>
+      <p>{property.description}</p>
+    </section>
+  )
 }
 
 export default Housing
