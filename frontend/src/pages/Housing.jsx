724cb2037c6d3e5a2cfc2b87cf6d80b9295133c8
Joris Barbin <barbin.joris@hotmail.fr>
2026-05-27 17:44:49 +0200

    Display property details and rating

--- frontend/src/pages/Housing.jsx
+++ frontend/src/pages/Housing.jsx
@@ -31,6 +31,19 @@ function Housing() {
       <h1>{property.title}</h1>
       <p>{property.location}</p>
       <p>{property.description}</p>
+      <p>{property.host.name}</p>
+      <img src={property.host.picture} alt={property.host.name} />
+      {property.tags.map((tag) => (
+        <span key={tag} className="tagHousing">{tag}</span>
+      ))}
+      {[1, 2, 3, 4, 5].map((index) => (
+        <img className="star-icon"
+          key={index}
+          src={index <= Number(property.rating) ? "/star-active.png" : "/star-inactive.png"}
+          alt={index <= Number(property.rating) ? "Icône d'étoile pleine" : "Icône d'étoile vide"}
+        />
+      ))}
+
     </section>
   )
 }
