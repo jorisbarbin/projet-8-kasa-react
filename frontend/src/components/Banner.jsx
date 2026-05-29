42c6faaac3e786ff80fc57ce3c3e1c5a5126ac93
Joris Barbin <barbin.joris@hotmail.fr>
2026-05-29 15:20:20 +0200

    Style banner component

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -3,8 +3,8 @@ import '../styles/Banner.css'
 function Banner({ image, title }) {
   return (
     <section className="banner">
-      <img src={image} alt="Bannière Kasa" />
-      {title && <h1>{title}</h1>}
+      <img className='banner-image' src={image} alt="Bannière Kasa" />
+      {title && <h1 className='banner-title'>{title}</h1>}
     </section>
   )
 }
