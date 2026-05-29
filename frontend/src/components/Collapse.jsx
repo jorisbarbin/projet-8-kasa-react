c67d72aaae89ac9c4f0afef13a43fe5ee3827075
Joris Barbin <barbin.joris@hotmail.fr>
2026-05-29 15:33:20 +0200

    Style about page and collapses

--- frontend/src/components/Collapse.jsx
+++ frontend/src/components/Collapse.jsx
@@ -5,17 +5,16 @@ import { useState } from 'react'
 function Collapse({ title, content }) {
     const [isOpen, setIsOpen] = useState(false)
 
-    const icon = isOpen
-        ? <img src="/DropIcon.png" alt="Icône du collapse"
-            className={isOpen ? "collapse-icon collapse-icon-open" : "collapse-icon"}
-        />
-        : <img src="/DropIcon.png" alt="Icone de flèche vers le haut" />;
- 
     return (
         <section className="collapse">
-            <h2 onClick={() => setIsOpen(!isOpen)} style={{ cursor: 'pointer' }}>
-                {title} {icon}
-            </h2>
+                <h2 className="collapse-title" onClick={() => setIsOpen(!isOpen)}>
+                {title}
+                <img
+                    className={`collapse-image ${isOpen ? "collapse-icon-open" : ""}`}
+                    src="/DropIcon.png"
+                    alt="Icône du collapse"
+                />
+                </h2>
             {isOpen && (
                 <div className="collapse-content">
                     {content}
