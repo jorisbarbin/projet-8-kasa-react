c0bdd271827924c09b118831eb8eb2294f621e5d
Joris Barbin <barbin.joris@hotmail.fr>
2026-05-29 16:36:07 +0200

    Style error page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,7 +1,13 @@
 import '../styles/Error.css'
+import { Link } from 'react-router-dom'
 
 function Error() {
-  return <h1 className="title-error">Erreur 404</h1>
+  return (
+    <div className="error">
+      <h1 className="error-title">404</h1>
+      <p className="error-text">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error-link">Retourner sur la page d’accueil</Link>
+    </div>
+  )
 }
-
 export default Error
